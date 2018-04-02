123dbd3c84adf23e5efc7d9472419385c2ba044b
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-01 21:16:31 -0500

    Vistas usando *.html.ecr

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    render("index.slang")
+    #render("index.slang")
+    render("index.html.ecr", layout: "blank.html.ecr")
   end
 end
-
